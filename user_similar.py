0d05a28fcff8dbbfbcfe728268cf3795daab67dc
GRMDS <53190949+GRMDS@users.noreply.github.com>
2019-10-01 16:24:00 -0700

    Add files via upload

--- user_similar.py
+++ user_similar.py
@@ -1,48 +1,43 @@
 import pandas as pd
-import mysql.connector
 from similarity.normalized_levenshtein import NormalizedLevenshtein
 from sqlalchemy import create_engine
+from loaddata import load_table_from_MySQL
+from sklearn.metrics.pairwise import cosine_similarity
 
 
 class user_similarity(object):
     def __init__(self):
-        cnx = mysql.connector.connect(user = 'grmds054_analyst', password = 'Cmethods1', 
-                                  host = '146.66.69.53', database = 'grmds054_drup881')
-        db_cursor = cnx.cursor()
-        self.cursor = db_cursor
-        company_table = self.load_data(query = ('SELECT entity_id, field_company_value FROM dr_user__field_company'), column_name = ['user_id','company'])
-        univer_table = self.load_data(query = ('SELECT entity_id,field_university_value FROM dr_user__field_university'), column_name = ['user_id','university'])
-        position_table = self.load_data(query = ('SELECT entity_id,field_position_value FROM dr_user__field_position;'), column_name = ['user_id','position'])
-        self.user_table = company_table.merge(univer_table, how = 'outer', on = 'user_id')
-        self.user_table = self.user_table.merge(position_table, how = 'outer', on = 'user_id')
-        self.feature_weight = {'company':0.3, 'university': 0.5, 'position': 0.2}
+        x = load_table_from_MySQL(username = 'grmds054_analyst', password = 'Cmethods1')
+        wholetable, column_names = x.load_data('dr_rmds_users')
+        self.feature_weight = {'company':0.2, 'university': 0.4, 'position': 0.1, 'interest': 0.3}
         self.user_sim = pd.DataFrame(columns = ['user','sim_user','sim_score'])
+        self.interest_list = ['communication_services','consumer_discretionary','consumer_staples',
+                         'energy','financials','health_care','industrials','information_technology','materials',
+                         'real_estate','utilities']
+        self.user_table = wholetable[['uid','company','university','job_title']+self.interest_list]
+        
        
-    def load_data(self, query, column_name):
-        self.cursor.execute(query)
-        wholetable = pd.DataFrame(columns = column_name)
-        for tuples in self.cursor:
-            dic = {}
-            for i in range(len(column_name)):
-                dic[column_name[i]] = tuples[i]
-            wholetable = wholetable.append(dic,ignore_index = True)
-        return wholetable
     
     def compare(self, uid1, uid2):
-        info1 = self.user_table.loc[self.user_table['user_id']==uid1,['company','university','position']].to_dict(orient = 'lists')
-        info2 = self.user_table.loc[self.user_table['user_id']==uid2,['company','university','position']].to_dict(orient = 'lists')
+        info1 = self.user_table.loc[self.user_table['uid']==uid1,['company','university','position']].to_dict(orient = 'lists')
+        info2 = self.user_table.loc[self.user_table['uid']==uid2,['company','university','position']].to_dict(orient = 'lists')
         normalized_levenshtein = NormalizedLevenshtein()
-        dist = 0
-        upper = 1
+        interest1 = self.user_table.loc[self.user_table['uid']==uid1,self.interest_list].values
+        interest2 = self.user_table.loc[self.user_table['uid']==uid2,self.interest_list].values
+        sim = 0
+        upper = 1-self.feature_weight['interest']
         for key in self.feature_weight.keys():
-            if type(info1[key][0]) is str and type(info2[key][0]) is str:
-                dist += self.feature_weight[key]*normalized_levenshtein.similarity(info1[key][0], info2[key][0])
+            if key=='interest':
+                sim += self.feature_weight[key]*cosine_similarity(interest1,interest2)[0][0]                
             else:
-                upper = upper - self.feature_weight[key]
-        return dist/upper
+                if type(info1[key][0]) is str and type(info2[key][0]) is str:
+                    sim += self.feature_weight[key]*normalized_levenshtein.similarity(info1[key][0], info2[key][0])
+                else:
+                    upper = upper - self.feature_weight[key]
+        return sim/upper
                     
     def compute_sim_matrix(self):
-        user_list = list(self.user_table['user_id'])
+        user_list = list(self.user_table['uid'])
         for user1 in user_list:
             lists = []
             for user2 in user_list:
