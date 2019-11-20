78ba2ef24262cf8bdac3c6fe90bec9a095b74851
GRMDS <jidongli@DESKTOP-OEOIVK6.localdomain>
2019-11-20 15:07:29 -0800

    output_format

--- pyserver.py
+++ pyserver.py
@@ -3,11 +3,11 @@ app = Flask(__name__)
 
 @app.route('/update_recommendation/<int:uid>')
 def update_recommendation(uid):
-    return uid*5
+    return str(uid*5)
 
 @app.route('/update_user_sim/<int:uid>')
 def update_user_sim(uid):
-    return uid/5
+    return str(uid/5)
     
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
