24f6f53cd5fc67a179b792484b36fd42daf66aac
GRMDS <jidongli@DESKTOP-OEOIVK6.localdomain>
2019-11-20 14:54:08 -0800

    para_test

--- pyserver.py
+++ pyserver.py
@@ -1,6 +1,13 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/update_recommendation/<int:uid>')
+def update_recommendation(uid):
+    return uid*5
+
+@app.route('/update_user_sim/<int:uid>')
+def update_user_sim(uid):
+    return uid/5
+    
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
